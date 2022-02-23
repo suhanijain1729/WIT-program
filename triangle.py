4ac1b89a41ea29b8f4c32be2b666d34793d1498f
suhanijain <99428236+suhanijain1729@users.noreply.github.com>
2022-02-23 10:03:35 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,16 @@
+a=input("Enter the length of first side ")
+b=input("Enter the length of second side")
+c=input("Enter the length of third side")
+
+if a==b==c:
+
+    print("Equilateral Triangle")
+
+elif a==b:
+
+    print("Isosceles Triangle")
+
+else:
+
+    print("Scaler Triangle")
+
