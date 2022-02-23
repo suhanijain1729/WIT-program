8bb02470f050878322930708f189771eeb07d346
suhanijain <99428236+suhanijain1729@users.noreply.github.com>
2022-02-23 08:51:18 +0530

    Add files via upload

---
+++ raindrop.py
@@ -0,0 +1,15 @@
+def convert(number):
+    output = ''
+    if number % 3 == 0:
+        output += 'Pling'
+
+    if number % 5 == 0:
+        output += 'Plang'
+
+    if number % 7 == 0:
+        output += 'Plong'
+
+    if output is '':
+        return str(number)
+    
+    return output
